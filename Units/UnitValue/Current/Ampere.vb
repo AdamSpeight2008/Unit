02ea84d3e74b016d7735f63a49d375679023d74c
AdamSpeight2008 <adam.speight@virgin.net>
2017-11-14 00:51:23 +0000

    Added Electric.Current

---
+++ Units/UnitValue/Current/Ampere.vb
@@ -0,0 +1,23 @@
+﻿
+Imports Units.Units.Electric.Current
+
+Namespace Global.Units.UnitValue.Electric.Current
+
+    Partial Public NotInheritable Class Ampere : Inherits AbstractUnitValue(Of Units.Electric.Current.Ampere)
+
+        Public Sub New(Value As Decimal)
+            MyBase.New(Value, Global.Units.Electric.Current.Ampere.Instance)
+        End Sub
+
+        Friend Overrides Function CreateNew(Value As Decimal) As AbstractUnitValue(Of Units.Electric.Current.Ampere)
+            Return New Ampere(Value)
+        End Function
+
+        Public Shared Widening Operator CType(x As Decimal) As Ampere
+            Return New Ampere(x)
+        End Operator
+
+
+    End Class
+
+End Namespace
---
+++ Units/Units/Electric/Current/Ampere.vb
@@ -0,0 +1,14 @@
+﻿Imports Units.UnitValue.Electric.Current
+
+Namespace Global.Units.Units.Electric.Currents
+
+    Partial Public NotInheritable Class Ampere : Inherits AbstractCurrentUnits
+
+        Public Shared ReadOnly Property Instance As New Ampere
+        Private Sub New()
+            MyBase.New("A", "amperes")
+        End Sub
+
+    End Class
+
+End Namespace
